f976ad2c6b20835f1a2748ae80c3aefbd53f6875
spyros <95617475+SpyrosLefkaditis@users.noreply.github.com>
2024-02-03 21:49:21 +0200

    Add files via upload

---
+++ seo_logic.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+url = "https://www.siteguru.co/free-seo-tools/onpage-seo-check?url=https%3A%2F%2Fwww.google.com%2F"
+
+# Set up the browser (make sure you have the appropriate webdriver installed)
+driver = webdriver.Chrome()
+driver.get(url)
+
+# Find and click the button
+button = driver.find_element(By.CSS_SELECTOR, ".button--white")
+button.click()
+
+# Add a delay to keep the browser open for 5 seconds (adjust as needed)
+time.sleep(20)
